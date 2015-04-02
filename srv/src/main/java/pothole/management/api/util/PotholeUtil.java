0bc254df68d45bf96f53d6103ca08238770ee440
brendanshort <brendan.short@digital.cabinet-office.gov.uk>
2015-04-02 13:47:42 +0100

    [bs]] modulated parsing code to make it more testable

--- srv/src/main/java/pothole/management/api/util/PotholeUtil.java
+++ srv/src/main/java/pothole/management/api/util/PotholeUtil.java
@@ -3,28 +3,44 @@ package pothole.management.api.util;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONObject;
 
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.Map;
 
 public class PotholeUtil {
     public static String parseGeoCode(String location){
         String locationClean = location.replaceAll(" ","+");
         try {
-            URL url = new URL("http://maps.googleapis.com/maps/api/geocode/json?address="+locationClean);
-            ObjectMapper mapper = new ObjectMapper();
-            JSONObject parsedJson = new JSONObject(mapper.readValue(url, Map.class));
-            JSONObject results = parsedJson.getJSONArray("results").getJSONObject(0);
-            JSONObject geometry = results.getJSONObject("geometry");
-            JSONObject loc = geometry.getJSONObject("location");
-            String lng = loc.get("lng").toString();
-            String lat = loc.get("lat").toString();
-            System.out.println("The geo location of " + location + " is: " +lat+ "," + lng);
+            JSONObject parsedJson = getLocationDetailsFromGoogle(locationClean);
+            Map<String, String> loc = getLatAndLongFromJsonObject(parsedJson);
 
-            return lat + "," + lng;
+            System.out.println("The geo location of " + location + " is: " + loc.get("lat") + "," + loc.get("lng"));
+
+            return loc.get("lat") + "," + loc.get("lng");
         }
         catch (Exception e){
             System.out.println(e);
             return "LOCATION NOT SET";
         }
+    }
+
+    public static JSONObject getLocationDetailsFromGoogle(String location) throws Exception {
+        URL url = new URL("http://maps.googleapis.com/maps/api/geocode/json?address=" + location);
+        ObjectMapper mapper = new ObjectMapper();
+        return new JSONObject(mapper.readValue(url, Map.class));
+    }
+
+    public static Map<String, String> getLatAndLongFromJsonObject(JSONObject data){
+        JSONObject results = data.getJSONArray("results").getJSONObject(0);
+        JSONObject geometry = results.getJSONObject("geometry");
+        JSONObject loc = geometry.getJSONObject("location");
+
+        HashMap<String, String> ret = new HashMap<>();
+
+        ret.put("lat", loc.get("lat").toString());
+        ret.put("lng", loc.get("lng").toString());
+
+        return ret;
     }
 }
